3d841bf303eef5ad8f827a79afcf6a27a380e598
unknown <nminhtue094@gmail.com>
2022-10-30 10:14:51 +0700

    sol

---
+++ Car.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self,make,model,year,color):
+        self.make=None
+        self.model=None
+        self.year=None
+        self.color=None
+        
+    def drive(self):
+        print("This car is driving")
+    def stop(self):
+        print("This car is stopped")
---
+++ main.py
@@ -0,0 +1,3 @@
+from Car import car
+
+car_1 =car("Chevi","Vinfast",2021,"blue")
